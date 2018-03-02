04349a0715744d4b6b4efb5731693bb179f2c4bb
darren <djodom@gmail.com>
2018-03-02 02:36:19 +0000

    Added new examples

--- examples/GstreamerOpenCV/simpleCV_Gstreamer.cpp
+++ examples/GstreamerOpenCV/simpleCV_Gstreamer.cpp
@@ -5,18 +5,28 @@
 */
 
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <sstream>
+
+template <typename T>
+std::string to_string(T value)
+{
+	std::ostringstream os ;
+	os << value ;
+	return os.str() ;
+}
 
 std::string get_tegra_pipeline(int width, int height, int fps) {
-    return "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(width) + ", height=(int)" +
-           std::to_string(height) + ", format=(string)I420, framerate=(fraction)" + std::to_string(fps) +
+    return "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)" + to_string(width) + ", height=(int)" +
+           to_string(height) + ", format=(string)I420, framerate=(fraction)" + to_string(fps) +
            "/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
 
 int main() {
     // Options
-    WIDTH = 1920;
-    HEIGHT = 1080;
-    FPS = 30;
+    int WIDTH = 1920;
+    int HEIGHT = 1080;
+    int FPS = 30;
 
     // Define the gstream pipeline
     std::string pipeline = get_tegra_pipeline(WIDTH, HEIGHT, FPS);
