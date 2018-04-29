c27d6f419464d8a631f98dae613a7689a0804a52
Darren Odom <D@sure.works>
2018-04-28 23:54:30 -0600

    Update build_kernel.sh

--- kernelpatch/build_kernel.sh
+++ kernelpatch/build_kernel.sh
@@ -1,8 +1,7 @@
-
 #!/bin/sh
 
 export TEGRA_BASE=/home/d/64_TX2/Linux_for_Tegra
-export CROSS_COMPILE=/opt/jetpack3.2/bin/aarch64-unknown-linux-gnu-
+export CROSS_COMPILE=$TEGRA_BASE/compiler/bin/aarch64-unknown-linux-gnu-
 echo $CROSS_COMPILE
 
 export TEGRA_KERNEL_OUT=$TEGRA_BASE/out/images
@@ -22,15 +21,16 @@ export LOCALVERSION=-HW4.2
 export TEGRA_X2_ROOTFS=$TEGRA_BASE/rootfs
 
 # If this is the first time, first do
+#Clean kernel config
+#make -C kernel/kernel-4.4/ mrproper
 # make O=$TEGRA_KERNEL_OUT ARCH=$ARCH tegra18_defconfig
 # and then edit the file $TEGRA_BASE/kernel/.config to enable drivers
 # you need.
 
-make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules_prepare
-make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH zImage
-make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH dtbs
-make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules
-make O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules_install INSTALL_MOD_PATH=$TEGRA_BAS$
+make -j 6 -C kernel/kernel-4.4/ O=$TEGRA_KERNEL_OUT ARCH=$ARCH zImage
+make -j 6 -C kernel/kernel-4.4/ O=$TEGRA_KERNEL_OUT ARCH=$ARCH dtbs
+make -j 6 -C kernel/kernel-4.4/ O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules
+make -C kernel/kernel-4.4/ O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules_install INSTALL_MOD_PATH=$TEGRA_BASE/rootfs
 
 
 cd $TEGRA_BASE/rootfs
@@ -38,4 +38,3 @@ tar cvfj $TEGRA_BASE/kernel/fresh-modules.tbz lib/modules
 cd $TEGRA_BASE
 cp kernel/arch/arm64/boot/dts/*.dtb kernel/dtb/
 ./apply_binaries.sh
-
