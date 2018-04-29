f571b3bea5023a3fa286a5e54f41735c17bbd7e6
Darren Odom <D@sure.works>
2018-04-28 18:31:00 -0600

    Create build_kernel.sh
    Created a kernel build script.

---
+++ kernelpatch/build_kernel.sh
@@ -0,0 +1,41 @@
+
+#!/bin/sh
+
+export TEGRA_BASE=/home/d/64_TX2/Linux_for_Tegra
+export CROSS_COMPILE=/opt/jetpack3.2/bin/aarch64-unknown-linux-gnu-
+echo $CROSS_COMPILE
+
+export TEGRA_KERNEL_OUT=$TEGRA_BASE/out/images
+echo $TEGRA_KERNEL_OUT
+
+export TEGRA_MODULES_OUT=$TEGRA_BASE/out/modules
+echo $TEGRA_MODULES_OUT
+
+
+mkdir -p $TEGRA_KERNEL_OUT
+mkdir -p $TEGRA_MODULES_OUT
+export ARCH=arm64
+
+#Insert the hardware number here:
+export LOCALVERSION=-HW4.2
+
+export TEGRA_X2_ROOTFS=$TEGRA_BASE/rootfs
+
+# If this is the first time, first do
+# make O=$TEGRA_KERNEL_OUT ARCH=$ARCH tegra18_defconfig
+# and then edit the file $TEGRA_BASE/kernel/.config to enable drivers
+# you need.
+
+make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules_prepare
+make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH zImage
+make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH dtbs
+make -j 6 O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules
+make O=$TEGRA_KERNEL_OUT ARCH=$ARCH modules_install INSTALL_MOD_PATH=$TEGRA_BAS$
+
+
+cd $TEGRA_BASE/rootfs
+tar cvfj $TEGRA_BASE/kernel/fresh-modules.tbz lib/modules
+cd $TEGRA_BASE
+cp kernel/arch/arm64/boot/dts/*.dtb kernel/dtb/
+./apply_binaries.sh
+
